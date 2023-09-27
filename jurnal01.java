d11f5c0f853524b9176a55660d8e5f9bcebc6637
Egi Danuajisantoso <egidanusantoso@gmail.com>
2023-09-27 14:42:59 +0700

    Update jurnal01.java
    first commit

--- jurnal01.java
+++ jurnal01.java
@@ -1,12 +1,19 @@
 public class jurnal01 {
     public static void main(String[] args) {
+        //menggunaka variable
         String nama = "Egi Danuajisantoso";
         String nim = "607062300074";
         int kelas = 01;
 
-        System.out.println("NAMA :"+nama);
-        System.out.println("NIM :"+nim);
+        System.out.println("NAMA  :"+nama);
+        System.out.println("NIM   :"+nim);
         System.out.println("KELAS :"+kelas);
+
+        System.out.println("----------------------------------");
         
+        //menggunakan sysout
+        System.out.println("NAMA  :"+"Egi Danuajisantoso");
+        System.out.println("NIM   :"+"607062300074");
+        System.out.println("KELAS :"+"D3RPLA-47-01");
     }
 }
