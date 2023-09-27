263731c05ce56f475aa0518c83dd8c70ec4a95d9
Egi Danuajisantoso <egidanusantoso@gmail.com>
2023-09-27 13:51:39 +0700

    first commit

---
+++ jurnal01.java
@@ -0,0 +1,12 @@
+public class jurnal01 {
+    public static void main(String[] args) {
+        String nama = "Egi Danuajisantoso";
+        String nim = "607062300074";
+        int kelas = 01;
+
+        System.out.println("NAMA :"+nama);
+        System.out.println("NIM :"+nim);
+        System.out.println("KELAS :"+kelas);
+        
+    }
+}
